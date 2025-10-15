071062af4079c8009793f4f3e95e2e2d3b8d39c3
Roland HUON <huonroland91@gmail.com>
2025-10-15 22:55:01 +0200

    update routes for comments

--- backend/routes/commentRoutes.js
+++ backend/routes/commentRoutes.js
@@ -3,7 +3,6 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/userModel");
 const Comment = require("../models/commentModel");
-const { v4: uuidv4 } = require("uuid");
 
 router.get('/', async (req, res) => {
     const comments = await Comment.find();
@@ -11,20 +10,30 @@ router.get('/', async (req, res) => {
 });
 
 router.get('/:uuid', async (req, res) => {
-    const comment = await Comment.findOne({ uuid: req.params.uuid });
-    res.json(comment);
+    const comment = await Comment.findOne({ uuid: req.params.uuid })
+        .populate("createdBy", "uuid firstName lastName avatar")
+        .populate("profile", "uuid firstName lastName avatar");
+    res.status(200).json(comment);
 });
 
 router.get('/profile/:uuid', async (req, res) => {
-    const profile = await User.findOne({ uuid: req.params.uuid });
-    const comments = await Comment.find({ profile: profile});
-    res.json(comments);
+    try {
+        const profile = await User.findOne({ uuid: req.params.uuid });
+        const comments = await Comment.find({ profile: profile.uuid})
+            .populate("createdBy", "uuid firstName lastName avatar")
+            .sort({ createdAt: -1 }); // à modifier pour augmenter la vitesse de la requête
+        res.status(200).json(comments);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
 });
 
 router.post('/', async (req, res) => {
     const { message, createdBy, profile } = req.body;
     if(!message || !createdBy || !profile) return res.status(400).json({ message: 'Missing required fields' });
     try {
+        const createdBy = await User.findOne({ uuid: createdBy });
+        const profile = await User.findOne({ uuid: profile });
         const newComment = await Comment.create({ uuid: uuidv4(), message, createdBy, profile });
         res.status(201).json(newComment);
     } catch (err) {
