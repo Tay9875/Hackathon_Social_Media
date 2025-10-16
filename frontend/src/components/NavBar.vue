033099af4c407bd3fca7ed41b6a64872a9417fc7
Thomas Marques <thomas.marques@edu.devinci.fr>
2025-10-16 14:27:44 +0200

    UDPATE navbar

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -1,9 +1,5 @@
 <script setup>
-import { ref } from "vue";
 import { RouterLink } from "vue-router";
-import { Bars3Icon } from "@heroicons/vue/24/outline";
-
-const mobileMenuOpen = ref(false);
 </script>
 
 <template>
