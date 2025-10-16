3c12f6966441787001bbe766eae6e79711b71971
Talyha <moreau.talyha@gmail.com>
2025-10-16 10:12:13 +0200

    deleting user <ith api router

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,16 +3,19 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/userModel");
 
+//retrieve all users
 router.get('/', async (req, res) => {
     const users = await User.find();
     res.json(users);
 });
 
+//retrieve user with uuid
 router.get('/:uuid', async (req, res) => {
     const user = await User.findOne({ uuid: req.params.uuid });
     res.json(user);
 });
 
+//create user
 router.post('/', async (req, res) => {
     const { firstName, lastName, gender, birthDate, email, adress, avatar, password, description } = req.body;
     if(!firstName || !lastName || !birthDate || !email || !password) return res.status(400).json({ message: 'Missing required fields' });
@@ -24,12 +27,23 @@ router.post('/', async (req, res) => {
     }
 });
 
+//modify user
 router.put('/:uuid', async (req, res) => {
     const { firstName, lastName, gender, birthDate, adress, avatar, password, description } = req.body;
     const { uuid } = req.params;
     try {
         const updatedUser = await User.updateOne({ uuid }, { firstName, lastName, gender, birthDate, adress, avatar, password, description });
         res.status(200).json(updatedUser);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+})
+
+//delete user
+router.delete('/:uuid', async (req, res) => {
+    try {
+        const deletedUser = await User.deleteOne({ uuid: req.params.uuid });
+        res.status(200).json(deletedUser);
     } catch (err) {
         res.status(500).json({ error: err.message });
     }
